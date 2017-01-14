23656b33dd632c3c6acbca3c28dc73c3e4cd5bab
jgerschler <jjgerschler@gmail.com>
2017-01-14 16:23:05 -0600

    updated

--- ExamGenerator.py
+++ ExamGenerator.py
@@ -4,42 +4,42 @@ import random, pyqrcode, copy, simplecrypt, os, argparse
 from fpdf import FPDF
 from binascii import hexlify
 
-class generateExam(object):
-    def __init__(self, numQuestions, numVersions):
-        self.numQuestions = numQuestions
-        self.numVersions = numVersions
-        self.questionList = []
-        self.questionDict = {0:'a',1:'b',2:'c',3:'d'}
+class GenerateExam(object):
+    def __init__(self, num_questions, num_versions):
+        self.num_questions = num_questions
+        self.num_versions = num_versions
+        self.question_list = []
+        self.question_dict = {0:'a',1:'b',2:'c',3:'d'}
 
     def input_q_and_a(self):
-        for i in range(self.numQuestions):
+        for i in range(self.num_questions):
             question = raw_input('Please type question ' + str(i+1) + '.')
             answer = raw_input('Please type the answer.')
-            filler1 = raw_input('Please type filler one.')
-            filler2 = raw_input('Please type filler two.')
-            filler3 = raw_input('Please type filler three.')
+            filler_0 = raw_input('Please type filler one.')
+            filler_1 = raw_input('Please type filler two.')
+            filler_2 = raw_input('Please type filler three.')
             
-            self.answerList = ['&3#'+answer, filler1, filler2, filler3]# obviously exam answers cannot contain string &3# -- this needs to be updated!
-            self.questionList.append([question,self.answerList])
-            self.answerList = []
+            self.answer_list = ['&3#'+answer, filler_0, filler_1, filler_2]# obviously exam answers cannot contain string &3# -- this needs to be updated!
+            self.question_list.append([question,self.answer_list])
+            self.answer_list = []
             
     def build_exam(self):
-        for i in range(self.numVersions):
-            self.newQuestionList = copy.deepcopy(self.questionList)
-            for m in range(len(self.newQuestionList)):
-                random.shuffle(self.newQuestionList[m][1])
-            random.shuffle(self.newQuestionList)
+        for i in range(self.num_versions):
+            self.new_question_list = copy.deepcopy(self.question_list)
+            for m in range(len(self.new_question_list)):
+                random.shuffle(self.new_question_list[m][1])
+            random.shuffle(self.new_question_list)
             
-            plainTextAnswerString = ''
-            for j in range(len(self.newQuestionList)):
-                plainTextAnswerString = plainTextAnswerString + str(j+1)
-                for k in range(len(self.newQuestionList[j][1])):
-                    if self.newQuestionList[j][1][k][:3] == '&3#':
-                        self.newQuestionList[j][1][k] = self.newQuestionList[j][1][k][3:]
-                        plainTextAnswerString = plainTextAnswerString + self.questionDict[k] + 'x'
+            plain_text_answer_string = ''
+            for j in range(len(self.new_question_list)):
+                plain_text_answer_string = plain_text_answer_string + str(j+1)
+                for k in range(len(self.new_question_list[j][1])):
+                    if self.new_question_list[j][1][k][:3] == '&3#':
+                        self.new_question_list[j][1][k] = self.new_question_list[j][1][k][3:]
+                        plain_text_answer_string = plain_text_answer_string + self.question_dict[k] + 'x'
                         
-            cipherText = hexlify(simplecrypt.encrypt('ChangeThisKey!',plainTextAnswerString))# encrypt answer string and convert binary to hex before making qr code
-            qr_code = pyqrcode.create(cipherText, error='M', version=14, mode='binary')# generate QR code
+            cipher_text = hexlify(simplecrypt.encrypt('ChangeThisKey!',plain_text_answer_string))# encrypt answer string and convert binary to hex before making qr code
+            qr_code = pyqrcode.create(cipher_text, error='M', version=14, mode='binary')# generate QR code
             qr_code.png('qrcode'+str(i)+'.png', scale=2, module_color=[0, 0, 0, 0], background=[0xff, 0xff, 0xff])# save as PNG  
             pdf=FPDF()
             pdf.alias_nb_pages()
@@ -52,17 +52,17 @@ class generateExam(object):
             pdf.image('gradebox.png',180,24,15)
             pdf.image('grid.png',60,22,110)
             pdf.set_font('Times','',8)
-            for j in range(len(self.newQuestionList)):
-                    pdf.multi_cell(0,5,str(j+1)+'. '+self.newQuestionList[j][0],0,1)
-                    pdf.multi_cell(0,7,'A. '+self.newQuestionList[j][1][0]+'          '\
-                             'B. '+self.newQuestionList[j][1][1]+'          '\
-                             'C. '+self.newQuestionList[j][1][2]+'          '\
-                             'D. '+self.newQuestionList[j][1][3]+'          ',0,1
+            for j in range(len(self.new_question_list)):
+                    pdf.multi_cell(0,5,str(j+1)+'. '+self.new_question_list[j][0],0,1)
+                    pdf.multi_cell(0,7,'A. '+self.new_question_list[j][1][0]+'          '\
+                             'B. '+self.new_question_list[j][1][1]+'          '\
+                             'C. '+self.new_question_list[j][1][2]+'          '\
+                             'D. '+self.new_question_list[j][1][3]+'          ',0,1
                              )
             pdf.output('exam'+str(i)+'.pdf','F')            
             
     def cleanup(self):
-        for i in range(self.numVersions):
+        for i in range(self.num_versions):
             try:
                 os.remove('qrcode'+str(i)+'.png')
             except:
@@ -80,8 +80,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    NewExam = generateExam(int(args.questions), int(args.versions))
-    NewExam.run()
+    new_exam = GenerateExam(int(args.questions), int(args.versions))
+    new_exam.run()
 
     print("")
     print("Done.")
